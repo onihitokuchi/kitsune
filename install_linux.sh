185f68cc883023ce3e70d18f7b91fd3aa4123008
Santos Vilanculos <santosvilanculos@yahoo.com>
2024-10-08 14:01:24 +0200

    chore: bump fastfetch version, add more games to be removed

--- install_linux.sh
+++ install_linux.sh
@@ -4,21 +4,33 @@ DIRNAME=$(dirname $(realpath "$0"))
 
 sudo apt-get purge -y ace-of-penguins
 sudo apt-get purge -y aisleriot
+sudo apt-get purge -y five-or-more
+sudo apt-get purge -y four-in-a-row
 sudo apt-get purge -y gbrainy
+sudo apt-get purge -y gnome-2048
 sudo apt-get purge -y gnome-chess
+sudo apt-get purge -y gnome-klotski
+sudo apt-get purge -y gnome-mahjongg
 sudo apt-get purge -y gnome-mines
+sudo apt-get purge -y gnome-nibbles
 sudo apt-get purge -y gnome-robots
 sudo apt-get purge -y gnome-sudoku
 sudo apt-get purge -y gnome-sushi
 sudo apt-get purge -y gnome-taquin
 sudo apt-get purge -y gnome-tetravex
 sudo apt-get purge -y gnomine
+sudo apt-get purge -y hitori
+sudo apt-get purge -y iagno
 sudo apt-get purge -y lightsoff
 sudo apt-get purge -y mahjongg
+sudo apt-get purge -y pegsolitaire
 sudo apt-get purge -y quadrapassel
 sudo apt-get purge -y swell-foop
-sudo apt-get clean -y
+sudo apt-get purge -y tali
+
 sudo apt-get autoremove -y
+sudo apt-get autoclean -y
+sudo apt-get clean -y
 sudo service packagekit restart
 
 sudo apt-get update
@@ -32,11 +44,19 @@ sudo apt-get install -y software-properties-common
 sudo apt-get install -y gpg
 sudo apt-get install -y build-essential
 sudo apt-get install -y curl wget
-sudo apt-get install -y zip unzip
-sudo apt-get install -y git gh
-sudo apt-get install -y ffmpeg vlc
-sudo apt-get install -y maim xsel xclip
-sudo apt-get install -y libnss3-tools jq openssl ca-certificates
+sudo apt-get install -y zip
+sudo apt-get install -y unzip
+sudo apt-get install -y git
+sudo apt-get install -y gh
+sudo apt-get install -y ffmpeg
+sudo apt-get install -y vlc
+sudo apt-get install -y maim
+sudo apt-get install -y xsel
+sudo apt-get install -y xclip
+sudo apt-get install -y libnss3-tools
+sudo apt-get install -y jq
+sudo apt-get install -y openssl
+sudo apt-get install -y ca-certificates
 
 sudo apt-get install -y zsh
 sudo chsh -s $(which zsh)
@@ -45,14 +65,11 @@ sudo apt install -y flatpak
 sudo apt install -y gnome-software-plugin-flatpak
 sudo flatpak remote-add --if-not-exists flathub "https://dl.flathub.org/repo/flathub.flatpakrepo"
 
-sudo flatpak install flathub it.mijorus.gearlever
+sudo flatpak install -y flathub it.mijorus.gearlever
 
-wget "https://github.com/fastfetch-cli/fastfetch/releases/download/2.21.0/fastfetch-linux-amd64.deb" -O "$DIRNAME/fastfetch-linux-amd64.deb"
-sudo dpkg -i "$DIRNAME/fastfetch-linux-amd64.deb"
-
-#wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O "$DIRNAME/google-chrome-stable_current_amd64.deb"
-#sudo dpkg -i "$DIRNAME/google-chrome-stable_current_amd64.deb"
-xdg-settings set default-web-browser google-chrome.desktop
+wget "https://github.com/fastfetch-cli/fastfetch/releases/download/2.27.1/fastfetch-linux-amd64.deb" -O "$DIRNAME/tmp/fastfetch-linux-amd64.deb"
+sudo dpkg -i "$DIRNAME/tmp/fastfetch-linux-amd64.deb"
 
 # https://extensions.gnome.org/extension/307/dash-to-dock/
-# flatpak install flathub io.github.zen_browser.zen
+
+mkdir -p $HOME/Software_Engineering/{bash,php,python}
